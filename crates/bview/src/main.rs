d598ac8b741089047dfe0b589b49f5810ea8c1dd
Uncle Scientist <unclescientist@gmail.com>
2023-04-13 08:37:02 -0400

    use nth instead of skip+next

--- crates/bview/src/main.rs
+++ crates/bview/src/main.rs
@@ -1,7 +1,7 @@
 use blorb::BlorbReader;
 
 fn main() {
-    let filename = std::env::args().skip(1).next().unwrap();
+    let filename = std::env::args().nth(1).unwrap();
     println!("reading file \"{filename}\"");
     let filedata = std::fs::read(filename).expect("unable to open file");
     let blorb = BlorbReader::new(filedata);
